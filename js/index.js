5178c62020c4c5a12b5fabaa98be3de0bc3dba89
knightflo <f.van.den.berg@sintlucasedu.nl>
2023-03-22 16:38:25 +0100

    done wow

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 document.getElementById("maintitle").innerText = "point and click adventures";
+const offsetCharacter = 16;
+const gamewindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gamewindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element
+    var y = e.clientY - rect.top; //y position within the element
+    
+    mainCharacter.style.left = x - offsetCharacter+"px";
+    mainCharacter.style.top = y - offsetCharacter+"px";
+
+    console.log(e.target.id);
+
+    switch(e.target.id){
+    case "doorone":
+        console.log("lets go trough");
+        break;
+    
+    case "doortwo":
+    console.log("there is nothing here");
+    break;
+    default:
+        console.log("there is nothing here")
+        break;
+    }
+}
