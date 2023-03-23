02997b59e1268d203cab4ed5928e1f0fbcb8b8e5
knightflo <f.van.den.berg@sintlucasedu.nl>
2023-03-23 16:50:52 +0100

    did many things

--- js/index.js
+++ js/index.js
@@ -13,14 +13,28 @@ gamewindow.onclick = function(e){
 
     console.log(e.target.id);
 
+function dectectcollision(one,two){
+    rect1=document.getElementById(one);
+    rect2=document.getElementById(two);
+
+    if (rect1.left < rect2.right && rect1.right > rect2.left &&
+        rect1.top < rect2.bottom && rect1.bottom > rect2.top) {
+            console.log("collision");
+        }
+}
+
     switch(e.target.id){
     case "doorone":
         console.log("lets go trough");
         break;
     
-    case "doortwo":
-    console.log("there is nothing here");
+    case "grass1":
+        document.getElementById("mainCharacter").style.left = "407px";
+        document.getElementById("mainCharacter").style.top = "494px";
+    console.log("died of grass poison");
     break;
+
+
     default:
         console.log("there is nothing here")
         break;
